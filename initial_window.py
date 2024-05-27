80d0db90f48a91198699c84eb3d6360be7db0296
Lukasz-Strugarek <120648183+Lukasz-Strugarek@users.noreply.github.com>
2024-05-27 12:30:16 +0200

    Creating and opening annotated files

--- initial_window.py
+++ initial_window.py
@@ -1,4 +1,5 @@
 import pickle
+import shutil
 import sys
 import os
 from PySide6.QtGui import QPainter, QPen, QColor, QPixmap
@@ -11,8 +12,11 @@ from tiff_splitter import split_tiff
 
 def load_profiles():
     if os.path.exists('profiles.pkl'):
-        with open('profiles.pkl', 'rb') as file:
-            return pickle.load(file)
+        try:
+            with open('profiles.pkl', 'rb') as file:
+                return pickle.load(file)
+        except EOFError:
+            return {}
     return {}
 
 def save_profiles(profiles):
@@ -89,9 +93,13 @@ class MainWindow(QMainWindow):
         # Funkcja obsługująca przycisk "Otwórz"
         options = QFileDialog.Options()
         file_name, _ = QFileDialog.getOpenFileName(self, "Otwórz plik", "", "Wszystkie pliki (*)", options=options)
-        if file_name:
+        base, _ = os.path.splitext(file_name)
+        anot_file_path = f"{base}_anot.pkl"
+        if file_name and os.path.exists(anot_file_path):
             self.anot_window = AnotationWindow(self, file_name)
             self.setCentralWidget(self.anot_window)
+        elif not os.path.exists(anot_file_path):
+            QMessageBox.warning(self, "Error", "NO ANNOTATION FILE FOUND")
 
 
 
@@ -101,42 +109,116 @@ class NewWindow(QWidget):
         self.setWindowTitle("New image")
         self.setGeometry(150, 150, 600, 200)
 
+        self.profiles = load_profiles()
+        print(self.profiles)
+
         layout = QVBoxLayout()
 
         # Tworzenie listy rozwijanej do wyboru trybu
         self.mode_selector = QComboBox(self)
-        self.mode_selector.addItems(["Tryb 1", "Tryb 2", "Tryb 3"])
+        self.mode_selector.addItems(list(self.profiles.keys()))
 
         #pole na ścieżkę
         self.path_input = QLineEdit(self)
-        self.path_input.setPlaceholderText("Wybierz ścieżkę zapisu")
+        self.path_input.setPlaceholderText("Wybierz ścieżkę obrazu")
         self.path_input.setReadOnly(True)
 
         # Tworzenie przycisku do wyboru ścieżki zapisu
         self.path_button = QPushButton("Wybierz ścieżkę")
-        self.path_button.clicked.connect(self.select_save_path)
+        self.path_button.clicked.connect(self.select_image_path)
 
         # Layout dla wyboru ścieżki
         path_layout = QHBoxLayout()
         path_layout.addWidget(self.path_input)
         path_layout.addWidget(self.path_button)
 
+        # pole na ścieżkę
+        self.destination_path_input = QLineEdit(self)
+        self.destination_path_input.setPlaceholderText("Wybierz ścieżkę zapisu")
+        self.destination_path_input.setReadOnly(True)
+
+        # Tworzenie przycisku do wyboru ścieżki zapisu
+        self.destination_path_button = QPushButton("Wybierz ścieżkę")
+        self.destination_path_button.clicked.connect(self.select_save_path)
+
+        # Layout dla wyboru ścieżki
+        destination_path_layout = QHBoxLayout()
+        destination_path_layout.addWidget(self.destination_path_input)
+        destination_path_layout.addWidget(self.destination_path_button)
+
+
+        self.confirm_button = QPushButton("Confirm")
+        self.confirm_button.clicked.connect(self.confirm)
+
         # Dodanie elementów do layoutu
         layout.addWidget(QLabel("Enter image type:"))
         layout.addWidget(self.mode_selector)
         layout.addWidget(QLabel("New image path:"))
         layout.addLayout(path_layout)
+        layout.addWidget(QLabel("Annotated image path:"))
+        layout.addLayout(destination_path_layout)
+        layout.addWidget(self.confirm_button)
 
         # Ustawienie layoutu dla głównego widgetu
         self.setLayout(layout)
 
-    def select_save_path(self):
+
+    def confirm(self):
+        if self.mode_selector != "" and self.destination_path_input.text() and self.path_input.text():
+            anot_list = {"type": self.mode_selector.currentText(),
+                         "annotations": []
+                        }
+
+            source_file_path = self.path_input.text()
+            destination_folder = self.destination_path_input.text()
+
+            if not os.path.isfile(source_file_path):
+                QMessageBox.warning(self, "Warning", "Source file does not exist!")
+                return
+
+                # Sprawdzenie, czy folder docelowy istnieje
+            if not os.path.isdir(destination_folder):
+                QMessageBox.warning(self, "Warning", "Destination folder does not exist!")
+                return
+
+
+            file_name = os.path.basename(source_file_path)
+            base_name, file_extension = os.path.splitext(file_name)
+
+            # Ścieżka do skopiowanego pliku
+            destination_file_path = os.path.join(destination_folder, file_name)
+
+            try:
+                # Kopiowanie pliku do folderu docelowego
+                shutil.copy2(source_file_path, destination_file_path)
+
+            except shutil.SameFileError:
+                QMessageBox.warning(self, "Error", "File already exists")
+
+            # Ścieżka do pliku z końcówką _anot.pkl
+            anot_file_path = os.path.join(destination_folder, f"{base_name}_anot.pkl")
+
+
+            with open(anot_file_path, 'wb') as anot_file:
+                pickle.dump(anot_list, anot_file)
+
+            QMessageBox.information(self, "Info", "Files have been successfully created!")
+
+
+
+    def select_image_path(self):
         # Funkcja otwierająca dialog do wyboru ścieżki zapisu
         options = QFileDialog.Options()
-        file_path, _ = QFileDialog.getSaveFileName(self, "Wybierz ścieżkę zapisu", "", "Wszystkie pliki (*)",
+        file_path, _ = QFileDialog.getOpenFileName(self, "Wybierz ścieżkę zapisu", "", "Wszystkie pliki (*)",
                                                    options=options)
         if file_path:
             self.path_input.setText(file_path)
+
+    def select_save_path(self):
+        options = QFileDialog.Options()
+        output_path = QFileDialog.getExistingDirectory(self, "Wybierz ścieżkę zapisu", options=options)
+        if output_path:
+            self.destination_path_input.setText(output_path)
 
 class TiffWindow(QWidget):
     def __init__(self, main_window):
@@ -495,6 +577,9 @@ class ProfileManager(QWidget):
         self.save_profile_button = QPushButton("Save Profile")
         self.save_profile_button.clicked.connect(self.save_profile)
 
+        self.cancel_profile_button = QPushButton("Cancel")
+        self.cancel_profile_button.clicked.connect(self.cancel_profile)
+
         layout.addWidget(self.profile_name_label)
         layout.addWidget(self.profile_name_entry)
         layout.addWidget(self.label_name_label)
@@ -502,6 +587,7 @@ class ProfileManager(QWidget):
         layout.addWidget(self.add_label_button)
         layout.addWidget(self.labels_listbox)
         layout.addWidget(self.save_profile_button)
+        layout.addWidget(self.cancel_profile_button)
 
         self.setLayout(layout)
 
